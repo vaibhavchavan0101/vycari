d6386d6c503b00726cd35977794b37335488585d
vaibhav chavan <vaibhav.c@amazatic.com>
2024-02-01 11:18:21 +0530

    Added models and custom authentication setup for user model.

---
+++ src/user/custom_user_manager.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for custom User model."""
+
+    def create_user(self, username, email, phone, password, **extra_fields):
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', False)
+        user = self.model(email=email, username=username, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, phone, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser permission.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active permisson.')
+        return self.create_user(username, email, phone, password, **extra_fields)
